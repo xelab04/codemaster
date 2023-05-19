072dd8551dbe34ec42286e44d97e2511c7884014
xelab04 <61658620+xelab04@users.noreply.github.com>
2023-05-19 21:31:58 +0400

    Add files via upload

---
+++ codex.php
@@ -0,0 +1,14 @@
+<?php
+//	$randomNumber = rand(1,10);
+//	echo ("The secret number to your success is: ");
+//	echo ($randomNumber);
+
+//	if ($randomNumber == 1)
+//	{
+//		header("Location: http://www.youtube.com");
+//	}
+
+//header("Location: ");
+  readfile("./index.html");
+	exit();
+?>
