241482d6f45f3edb9d941bcc54ec981dbdf6658e
xelab04 <61658620+xelab04@users.noreply.github.com>
2023-05-21 10:44:52 +0400

    Update codex.php

--- codex.php
+++ codex.php
@@ -7,8 +7,8 @@
 //	{
 //		header("Location: http://www.youtube.com");
 //	}
-
+	header("Location: https://xelab04.github.io/codemaster/index.html");
 //header("Location: ");
-  readfile("./index.html");
+//readfile("./index.html");
 	exit();
 ?>
